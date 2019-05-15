15af096bd7221c522d957d3949cb69270190dfbb
fathimalinsa <linsa@mozilor.com>
2019-05-15 13:29:27 +0530

    updated gulp version to 4.0.2 and jquery version to 3.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 CHANGELOG
 =========
 
+V 1.1.0
+-------
+ - Gulp update to v4
+ - Jquery version updated
+
 V 1.0.0
 -------
  - Initial release
--- template/gulpfile.js
+++ template/gulpfile.js
@@ -5,7 +5,6 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var del = require('del');
-var runSequence = require('run-sequence');
 var replace = require('gulp-replace');
 var injectPartials = require('gulp-inject-partials');
 var inject = require('gulp-inject');
@@ -19,24 +18,30 @@ gulp.paths = {
 
 var paths = gulp.paths;
 
-
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', gulp.series('sass', function() {
 
     browserSync.init({
-        port: 3000,
+        port: 3100,
         server: "./",
         ghostMode: false,
         notify: false
     });
 
-    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
     gulp.watch('**/*.html').on('change', browserSync.reload);
     gulp.watch('js/**/*.js').on('change', browserSync.reload);
 
-});
-
+}));
 
 
 // Static Server without watching scss files
@@ -55,28 +60,9 @@ gulp.task('serve:lite', function() {
 });
 
 
-
-gulp.task('sass', function () {
-    return gulp.src('./scss/**/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream());
-});
-
-
-
 gulp.task('sass:watch', function () {
     gulp.watch('./scss/**/*.scss');
 });
-
-
-/*sequence for injecting partials and replacing paths*/
-gulp.task('inject', function() {
-  runSequence('injectPartial' , 'injectCommonAssets' , 'injectLayoutStyles', 'replacePath');
-});
-
 
 
 /* inject partials like sidebar and navbar */
@@ -85,7 +71,6 @@ gulp.task('injectPartial', function () {
     .pipe(injectPartials())
     .pipe(gulp.dest("."));
 });
-
 
 
 /* inject Js and CCS assets into HTML */
@@ -100,8 +85,7 @@ gulp.task('injectCommonAssets', function () {
         './css/*.css', 
         './js/off-canvas.js', 
         './js/hoverable-collapse.js', 
-        './js/template.js', 
-        './js/todolist.js'
+        './js/template.js',
     ], {read: false}), {relative: true}))
     .pipe(gulp.dest('.'));
 });
@@ -117,26 +101,27 @@ gulp.task('injectLayoutStyles', function () {
 
 /*replace image path and linking after injection*/
 gulp.task('replacePath', function(){
-    gulp.src(['./pages/*/*.html'], { base: "./" })
+    var replacePath1 = gulp.src(['./pages/*/*.html'], { base: "./" })
         .pipe(replace('="images/', '="../../images/'))
         .pipe(replace('href="pages/', 'href="../../pages/'))
         .pipe(replace('href="documentation/', 'href="../../documentation/'))
         .pipe(replace('href="index.html"', 'href="../../index.html"'))
         .pipe(gulp.dest('.'));
-    gulp.src(['./pages/*.html'], { base: "./" })
+    var replacePath2 = gulp.src(['./pages/*.html'], { base: "./" })
         .pipe(replace('="images/', '="../images/'))
         .pipe(replace('"pages/', '"../pages/'))
         .pipe(replace('href="index.html"', 'href="../index.html"'))
         .pipe(gulp.dest('.'));
-    gulp.src(['./index.html'], { base: "./" })
+    var replacePath3 = gulp.src(['./index.html'], { base: "./" })
         .pipe(replace('="images/', '="images/'))
         .pipe(gulp.dest('.'));
+    return merge(replacePath1, replacePath2, replacePath3);
 });
 
-/*sequence for building vendor scripts and styles*/
-gulp.task('bundleVendors', function() {
-    runSequence('clean:vendors','buildBaseVendorStyles','buildBaseVendorScripts','copyRecursiveVendorFiles');
-});
+/*sequence for injecting partials and replacing paths*/
+gulp.task('inject', gulp.series('injectPartial' , 'injectCommonAssets' , 'injectLayoutStyles', 'replacePath'));
+
+
 
 gulp.task('clean:vendors', function () {
     return del([
@@ -164,18 +149,22 @@ gulp.task('buildBaseVendorStyles', function() {
 });
 
 gulp.task('copyRecursiveVendorFiles', function() {
-    gulp.src(['./node_modules/chart.js/dist/Chart.min.js'])
+    var vFile1 = gulp.src(['./node_modules/chart.js/dist/Chart.min.js'])
         .pipe(gulp.dest('./vendors/chart.js'));
-    gulp.src(['./node_modules/datatables.net/js/jquery.dataTables.js'])
+    var vFile2 = gulp.src(['./node_modules/datatables.net/js/jquery.dataTables.js'])
         .pipe(gulp.dest('./vendors/datatables.net'));
-    gulp.src(['./node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js'])
+    var vFile3 = gulp.src(['./node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js'])
         .pipe(gulp.dest('./vendors/datatables.net-bs4'));
-    gulp.src(['./node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css'])
+    var vFile4 = gulp.src(['./node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css'])
         .pipe(gulp.dest('./vendors/datatables.net-bs4'));
-    gulp.src(['./node_modules/@mdi/font/css/materialdesignicons.min.css'])
+    var vFile5 = gulp.src(['./node_modules/@mdi/font/css/materialdesignicons.min.css'])
         .pipe(gulp.dest('./vendors/mdi/css'));
-    gulp.src(['./node_modules/@mdi/font/fonts/*'])
+    var vFile6 = gulp.src(['./node_modules/@mdi/font/fonts/*'])
         .pipe(gulp.dest('./vendors/mdi/fonts'));
+    return merge(vFile1, vFile2, vFile3, vFile4, vFile5, vFile6);
 });
 
-gulp.task('default', ['serve']);
+/*sequence for building vendor scripts and styles*/
+gulp.task('bundleVendors', gulp.series('clean:vendors','buildBaseVendorStyles','buildBaseVendorScripts','copyRecursiveVendorFiles'));
+
+gulp.task('default', gulp.series('serve'));
